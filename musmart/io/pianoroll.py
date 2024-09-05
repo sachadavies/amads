522e47e78b6177c98478c35d6de548e007c4e272
ShepherdGH <yuxinmiao222@gmail.com>
2024-09-05 14:43:36 -0400

    Changed implementation in pianoroll.py

--- musmart/io/pianoroll.py
+++ musmart/io/pianoroll.py
@@ -4,10 +4,11 @@ Ports `pianoroll` Function
 Original Doc: https://citeseerx.ist.psu.edu/document?repid=rep1&type=pdf&doi=6e06906ca1ba0bf0ac8f2cb1a929f3be95eeadfa#page=82
 """
 
-from basics import Score, Note
-import matplotlib.figure as figure, matplotlib.pyplot as plt
-import matplotlib.patches as patches
+import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib import figure
+from matplotlib import patches
+from musmart import Score, Note
 
 
 def midi_num_to_name(midi_num: int, accidental) -> str:
@@ -18,20 +19,26 @@ def midi_num_to_name(midi_num: int, accidental) -> str:
 
     Args:
         midi_num (int): The midi number to be converted
-        accidental (str): If the note has an accidental, 
-                    determines if it is a sharp or a flat.
-                        Defaults to 'sharp', raises exception for
-                        inputs that's not either 'sharp' or 'flat'
-        
+        accidental (str): If the note has an accidental, determines if
+        it is a sharp or a flat. Valid input: 'sharp' or 'flat'.
+
+    Returns:
+        A string representing the name of the note that matches the
+        input MIDI number. 
+            
+    Raises:
+        ValueError: If there are invalid input argument.
     """
 
     octave = str(int((midi_num / 12) - 1))
-    
+
     match accidental:
         case 'sharp':
-            base = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'][midi_num % 12]
+            base = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#',
+                    'G', 'G#', 'A', 'A#', 'B'][midi_num % 12]
         case 'flat':
-            base = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B'][midi_num % 12]
+            base = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb',
+                    'G', 'Ab', 'A', 'Bb', 'B'][midi_num % 12]
 
     return base + octave
 
@@ -39,65 +46,85 @@ def midi_num_to_name(midi_num: int, accidental) -> str:
 def pianoroll(score: Score, y_label='name', x_label='beat',
               color='skyblue', accidental='sharp') -> figure.Figure:
     """
-    Returns a matplotlib.figure.Figure which displays a piano roll of a musical score.
+    Converts a Score to a piano roll display of a musical score.
 
     Args:
         score (Score): The musical score to display
         y_label (str, optional): Determines whether the y-axis is 
-                                 labeled with note names or MIDI numbers
-                                    Raises exception on inputs that's not
-                                    'name' or 'num'.
-        x_label (str, optional): Determines whether the x-axis is labeled 
-                                 with beats or seconds.
-                                    Raises exception on inputs that's not
-                                    'beat' or 'sec'.
-        accidental (str, optional): Determines whether the y-axis is labeled 
-                                    with sharps or flats. Only useful if argument
-                                    y_label is 'name'
-                                        Raises exception on inputs that's not
-                                        'sharp' or 'flat'.
+            labeled with note names or MIDI numbers.
+            Valid Input: 'name' or 'num'.
+        x_label (str, optional): Determines whether the x-axis is labeled
+            with beats or seconds. Valid input: 'beat' or 'sec'.
+        accidental (str, optional): Determines whether the y-axis is 
+            labeled with sharps or flats. O nly useful if argument
+            y_label is 'name'. Raises exception on inputs that's not
+            'sharp' or 'flat'.
+
+    Returns:
+        A matplotlib.figure.Figure of a pianoroll diagram. 
+
+    Raises:
+        ValueError: If there are invalid input argument. 
     """
 
-    # Stores properties of all notes in numpy array
-    note_list = score.find_all(Note)
-    note_info = np.array([[note.qstart(), note.dur, note.pitch.keynum] for note in note_list])
+    # Check for correct x_label input argument
+    if (x_label != 'beat' and x_label != 'sec'):
+        raise ValueError(
+            "Invalid x_label type"
+        )
 
-    midi_numbers = list(range(int(np.min(note_info[:, 2]) - 1), int(np.max(note_info[:, 2]) + 1)))
-    
+    fig, ax = plt.subplots()
+
+    min_note, max_note = 127.0, 0.0
+    max_time = 0
+    for note in score.strip_ties().find_all(Note):
+        start_time = note.qstart()
+        pitch = note.keynum - 0.5
+        duration = note.dur
+        
+        # Conditionally converts beat to sec
+        if x_label == 'sec':
+            start_time = score.timemap.beat_to_time(start_time)
+            duration = score.timemap.beat_to_time(duration)
+
+        # Stores min and max note for y_axis labeling
+        if (pitch < min_note):
+            min_note = pitch
+        if (pitch > max_note):
+            max_note = pitch
+        
+        # Stores max note start time + note duration for x_axis limit
+        if (start_time + duration > max_time):
+            max_time = start_time + duration
+
+        # Draws the note
+        rect = patches.Rectangle((start_time, pitch), duration, 1,
+                                 edgecolor='black', facecolor=color)
+        ax.add_patch(rect)
+
+    # Determines correct axis labels
+    midi_numbers = list(
+        range(int(min_note), int(max_note + 2)))
+
     match y_label:
         case 'num':
             notes = midi_numbers
         case 'name':
             notes = [midi_num_to_name(mn, accidental) for mn in midi_numbers]
         case _:
-            Exception(
+            raise ValueError(
                 "Invalid y_label type"
             )
 
-    if x_label == 'sec':
-        vectorized_beat_to_time = np.vectorize(score.timemap.beat_to_time)
-        
-        note_info[:, 0] = vectorized_beat_to_time(note_info[:, 0])
-        note_info[:, 1] = vectorized_beat_to_time(note_info[:, 1])
-    elif x_label != 'beat':
-        Exception(
-            "Invalid x_label type"
-        )
-
-    fig, ax = plt.subplots()
-
-    for i, (start_time, duration, note) in enumerate(zip(note_info[:, 0], note_info[:, 1], note_info[:, 2])):
-        rect = patches.Rectangle((start_time, note - 0.5), duration, 1, edgecolor='black', facecolor=color)
-        ax.add_patch(rect)
-
+    # Plots the graph
     ax.set_xlabel(x_label)
     ax.set_ylabel(y_label)
 
     ax.set_yticks(midi_numbers)
     ax.set_yticklabels(notes)
 
-    ax.set_xlim(0, max(note_info[:, 0]) + max(note_info[:, 1]))
+    ax.set_xlim(0, max_time)
     ax.set_ylim(min(midi_numbers), max(midi_numbers) + 1)
     ax.grid(True)
-    
+
     return fig
--- test/pianoroll_test.py
+++ test/pianoroll_test.py
@@ -1,9 +1,16 @@
 from matplotlib import pyplot as plt
-from partitura_midi_import import partitura_midi_import
+from musmart.pt_midi_import import partitura_midi_import
 from musmart.pianoroll import pianoroll
 from musmart import example
 
 my_midi_file = example.fullpath("midi/sarabande.mid")
+myscore = partitura_midi_import(my_midi_file, ptprint=False)
+
+pr_fig = pianoroll(myscore)
+plt.show() 
+
+
+my_midi_file = example.fullpath("midi/twochan.mid")
 myscore = partitura_midi_import(my_midi_file, ptprint=False)
 
 pr_fig = pianoroll(myscore)
