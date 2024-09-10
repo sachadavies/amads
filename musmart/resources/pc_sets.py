47d42fd920caf903caf8045c792e3e0345106a63
Mark Gotham <19486660+MarkGotham@users.noreply.github.com>
2024-09-10 11:59:41 +0100

    3-way split
    - resource (look up)
    - functions
    - tests

--- musmart/resources/pc_sets.py
+++ musmart/resources/pc_sets.py
@@ -14,9 +14,16 @@ Creative Commons Attribution-ShareAlike 4.0 International License
 https://creativecommons.org/licenses/by-sa/4.0/
 
 
+SOURCE:
+===============================
+Gotham and Yust, Serial Analyser, DLfM 2021
+https://github.com/MarkGotham/Serial_Analyser
+
+
 ABOUT:
 ===============================
-Properties of pitch class sets, and
+Look up tables for the properties of pitch class sets.
+Note: this toolkit also provides algorithmic
 functions for retrieving one property directly from another.
 
 Each pitch class set entry features the following properties:
@@ -33,26 +40,16 @@ from among 5 options:
 'RI' for retrograde-inversion only (13), and
 '' (an empty string) for non-combinatorial (16).
 
-Most of the retrieval function names are in the form
-`<call-on-type>To<return-type>`
-e.g.
-`prime_to_combinatoriality`
-Some are simple mappings from one entry to another.
-Anything starting with pitches involves more calculation.
-
 For more information on set classes and a more detailed list of properties, see
 Robert Morris's table and brief explanation (with further sources) here:
 http://ecmc.rochester.edu/rdm/pdflib/set-class.table.pdf
 """
 
-from typing import Union, List, Tuple
-import transformations
-import unittest
 
 # ------------------------------------------------------------------------------
 
 # PC set properties
-setClassesList = (
+set_classes = (
 
     (  # Cardinality 0 not supported
         None
@@ -318,191 +315,3 @@ setClassesList = (
     )
 
 )
-
-
-def set_classes_from_cardinality(cardinality: int):
-    """
-    In: a cardinality (2-10).
-    Out: the pitch class set data for that cardinality.
-    """
-    if not (1 < cardinality < 11):
-        raise ValueError('Invalid cardinality: must be 2-10 (inclusive).')
-    else:
-        return setClassesList[cardinality]
-
-
-def prime_to_combinatoriality(prime: Tuple[int]):
-    """
-    In: a prime form expressed as a Tuple of integers.
-    Out: the combinatoriality status as a string.
-    """
-    data = set_classes_from_cardinality(len(prime))
-    for x in data:
-        if x[1] == prime:
-            return x[3]
-    raise ValueError(f'{prime} is not a valid prime form')
-
-
-def interval_vector_to_combinatoriality(vector: Tuple[int]):
-    """
-    In: an interval vector for any set with 2-10 distinct pitches,
-    expressed as a Tuple of 6 integers.
-    Out: the combinatoriality status of any valid interval vector as a
-    string (one of T, I, RI, A, or an empty string for non-combinatorial cases).
-    """
-    if len(vector) != 6:
-        raise ValueError(f'{vector} is not a valid interval vector')
-    total = sum(vector)
-    total_to_cardinality = {
-        1: 2,
-        3: 3,
-        6: 4,
-        15: 6
-    }
-    data = set_classes_from_cardinality(total_to_cardinality[total])
-    for x in data:
-        if x[2] == vector:
-            return x[-1]
-    raise ValueError(f'{vector} is not a valid interval vector')
-
-
-def pitches_to_combinatoriality(pitches: Union[List[int], Tuple[int]]):
-    """
-    In: a list or tuple of pitches expressed as integers (0–11) for sets with 2-10 distinct pitches.
-    Out: the combinatoriality status as a string.
-    """
-    icv = pitches_to_interval_vector(pitches)
-    return interval_vector_to_combinatoriality(icv)
-
-
-def distinct_PCs(pitches: Union[List, Tuple]) -> list:
-    """
-    In: a list or tuple of pitches (any integers).
-    Out: a list of distinct PCs in the range 0-11.
-    """
-    pitches = list(set(pitches))  # remove any duplicates
-    return [p % 12 for p in pitches]
-
-
-def pitches_to_interval_vector(pitches: Union[List[int], Tuple[int]]):
-    """
-    In: a list or tuple of pitches.
-    Out: the interval vector.
-    """
-    pitches = distinct_PCs(pitches)
-
-    vector = [0, 0, 0, 0, 0, 0]
-    from itertools import combinations
-    for p in combinations(pitches, 2):
-        ic = p[1] - p[0]
-        if ic < 0:
-            ic *= -1
-        if ic > 6:
-            ic = 12 - ic
-        vector[ic - 1] += 1
-    return tuple(vector)
-
-
-def pitches_to_forte_class(pitches: Union[List[int], Tuple[int]]):
-    """
-    In: a list or tuple of pitches expressed as integers (0–11) for sets with 2-10 distinct pitches.
-    Out: the Forte class.
-    """
-    data = set_classes_from_cardinality(len(pitches))
-    prime = pitches_to_prime(pitches)
-    for x in data:
-        if x[1] == prime:
-            return x[0]
-    raise ValueError(f'{pitches} is not a valid entry.')
-
-
-def pitches_to_prime(pitches: Union[List[int], Tuple[int]]):
-    """
-    In: a list or tuple of pitches expressed as integers (0–11) for sets with 2-10 distinct pitches.
-    Out: the prime form.
-
-    The function first converts the pitches to their interval vector (easy, fast).
-    That vector unambiguously gives the prime form for cases except those with Z-related pairs.
-    This affects one pair of tetrachords (so 2 prime forms) and 15 pairs of hexachords (30 primes).
-
-    In those cases, the prime form is worked out by comparing the pitch list against the pair of
-    options in both inversions until a match is found.
-    """
-
-    pitches = distinct_PCs(pitches)
-
-    vector = pitches_to_interval_vector(pitches)
-    primes = []
-    data = set_classes_from_cardinality(len(set(pitches)))
-
-    for x in data:
-        if x[2] == vector:
-            primes.append(x[1])
-
-    if len(primes) == 1:
-        return primes[0]
-    elif len(primes) > 1:
-        for prime in primes:  # each possible prime form
-            inverted = transformations.invert(prime)
-            for t in [prime, inverted]:
-                if transposition_equivalent(t, pitches):
-                    return prime
-
-
-def transposition_equivalent(set1, set2):
-    """
-    Supporting function for determining whether two sets are transposition equivalent
-    as part of determining prime forms with `pitches_to_prime`.
-    """
-    sorted_set2 = sorted(list(set2))
-    for i in range(12):
-        test_case = sorted(list(transformations.transposeBy(set1, i)))
-        if test_case == sorted_set2:
-            return True
-
-
-# ------------------------------------------------------------------------------
-
-class PCTester(unittest.TestCase):
-
-    def test_pitches_to_prime(self):
-        """
-        Tests one case through the interval vector, and another that requires transformation.
-        """
-        prime = pitches_to_prime((0, 2, 3))
-        self.assertEqual(prime, (0, 1, 3))
-
-        # Test one case of numbers beyond 0-11
-        prime = pitches_to_prime((100, 102, 103))
-        self.assertEqual(prime, (0, 1, 3))
-
-        prime = pitches_to_prime((8, 2, 4, 7))  # via I [0,2,5,6], t2 [2,4,7,8], and shuffle.
-        self.assertEqual(prime, (0, 1, 4, 6))
-
-    def test_self_complement_hexachords(self):
-        """
-        Tests that all and only the hexachords without a Z-related pair are self-complementary.
-        (In so doing, this also tests the pitches-to-prime routine.)
-        """
-
-        count_hexachords = 0
-        count_total = 0
-        for entry in set_classes_from_cardinality(6):
-            hexachord = entry[1]
-            complement = tuple([x for x in range(12) if x not in hexachord])
-            complement_prime = pitches_to_prime(complement)
-            if hexachord == complement_prime:
-                self.assertFalse('Z' in entry[0])
-                count_hexachords += 1
-                count_total += entry[3]
-            else:
-                self.assertTrue('Z' in entry[0])
-
-        self.assertEqual(count_hexachords, 20)  # 20/50, so 40%
-        self.assertEqual(count_total, 372)  # 372/924, so 35.4%
-
-
-# ------------------------------------------------------------------------------
-
-if __name__ == '__main__':
-    unittest.main()
