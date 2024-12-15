ae2898b147b863391b866b2037f61ce63cd5f872
Peter Harrison <pmch2@cam.ac.uk>
2024-12-15 17:38:03 +0000

    Add note about durdist2 demo

--- tests/test_demos.py
+++ tests/test_demos.py
@@ -9,7 +9,9 @@ import pytest
 )
 def test_demos_run_without_errors(script):
     if script == "durdist2.py":
-        pytest.skip("Skipping durdist2 demo")
+        pytest.skip(
+            "Skipping durdist2 demo (see https://github.com/music-computing/amads/issues/43)"
+        )
 
     script_path = os.path.join("demos", script)
     runpy.run_path(script_path, run_name="__main__")
