6b3f5486c4e9e564fa21c27018f069deb312d5ca
Peter Harrison <pmch2@cam.ac.uk>
2025-03-01 21:47:56 +0000

    Specify collect_ignore in conftest.py (#77)

---
+++ conftest.py
@@ -0,0 +1,3 @@
+# This tells pytest not to try and collect tests from demos, docs, examples, or tests_to_fix.
+# Note that the demos are tested separately in tests/test_demos.py.
+collect_ignore = ["demos", "docs", "examples", "tests_to_fix"]
